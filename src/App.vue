4b118bdd9194b0eefa879b2ec54431ca93afcf73
Milan G <dev@milangress.de>
2020-04-30 17:19:38 +0200

    ⚡︎ some stying and coins 🦈

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <style lang="stylus">
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
+  font-family "Arial Narrow", Arial
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-  div.slotmachine
+  div(v-on:click="newRandomArray").slotmachine
     div.slots
       p {{luckyWords[0]}}
       p {{luckyWords[1]}}
       p {{luckyWords[2]}}
-    button(v-on:click="newRandomArray") Run
+    //button(v-on:click="newRandomArray") Run
 </template>
 
 <script>
@@ -16,7 +16,7 @@ export default {
   data: function () {
     return {
       luckyWords: ['', '', ''],
-      words: ['Moral', 'Interventions', 'Dark Patterns', 'Design', 'Possibility', 'Individual', 'Digital Wellness', 'Self–Help', 'Complexity ', 'World', 'Planet Earth ', 'Planet Earth ', 'Space', 'Religion', 'Religious', 'Spiritual', 'Schlaraffenland', 'Heaven', 'Art', 'Revolution', 'Voice', 'Commune', 'Progress', 'Humanity', 'Romantic', 'Relationship', 'Earthly Paradise', 'Paradise', 'Money', 'Commercialism', 'Capitalism', 'Socialism', 'New', 'Work', 'Freedom', 'Space', 'Faith', 'Harmony', 'A God', 'Cooperation', 'Whole', 'Souls', 'Spirituality', 'Nature', 'Sin', 'Living Standards', 'Optimism', 'Pessimism', 'Feminism', 'Critics', 'Extinction', 'Sustainability ', 'Human Conditions', 'Human Nature', 'Suffering', 'Death', 'Myths', 'Garden Eden', 'Happily', 'Critical Times ', 'Cultures', 'Countries', 'Societies', 'Memory', 'History', 'Past ', 'Future', 'Primitive', 'Dream', 'Premature Truth', 'Keep Shining ', 'Social Distancing ', 'Speculations', 'Epidemic ', 'Frequent ', 'Principles', 'Eternal Life ', 'Death Itself', 'Beginning', 'End ', 'Ridiculous', 'Impossible', 'Possible', 'Desire', 'Urge', 'Imagination', 'Technology', 'Infastructure', 'Attention', 'Empathy', 'Addictive', 'Unadressed', 'Adressed', 'Real–World', 'VR', 'Perfect User', 'Protest', 'Health', 'Creative', 'Medicine', 'Cure', 'Meditation', 'Balance', 'Social', 'Media', 'Hunger', 'Water', 'Crisis', 'Global', 'Local ', 'Market', 'Food', 'Feed', 'Family ', 'Single', 'Friendly']
+      words: ['Moral', 'Interventions', 'Dark Patterns', 'Design', 'Possibility', 'Individual', 'Digital Wellness', 'Self–Help', 'Complexity ', 'World', 'Planet Earth ', 'Planet Earth ', 'Space', 'Religion', 'Religious', 'Spiritual', 'Schlaraffenland', 'Heaven', 'Art', 'Revolution', 'Voice', 'Commune', 'Progress', 'Humanity', 'Romantic', 'Relationship', 'Earthly Paradise', 'Paradise', 'Money', 'Commercialism', 'Capitalism', 'Socialism', 'New', 'Work', 'Freedom', 'Space', 'Faith', 'Harmony', 'A God', 'Cooperation', 'Whole', 'Souls', 'Spirituality', 'Nature', 'Sin', 'Living Standards', 'Optimism', 'Pessimism', 'Feminism', 'Critics', 'Extinction', 'Sustainability ', 'Human Conditions', 'Human Nature', 'Suffering', 'Death', 'Myths', 'Garden Eden', 'Happily', 'Critical Times ', 'Cultures', 'Countries', 'Societies', 'Memory', 'History', 'Past ', 'Future', 'Primitive', 'Dream', 'Premature Truth', 'Keep Shining ', 'Social Distancing ', 'Speculations', 'Epidemic ', 'Frequent ', 'Principles', 'Eternal Life ', 'Death Itself', 'Beginning', 'End ', 'Ridiculous', 'Impossible', 'Possible', 'Desire', 'Urge', 'Imagination', 'Technology', 'Infastructure', 'Attention', 'Empathy', 'Addictive', 'Unadressed', 'Adressed', 'Real–World', 'VR', 'Perfect User', 'Protest', 'Health', 'Creative', 'Medicine', 'Cure', 'Meditation', 'Balance', 'Social', 'Media', 'Hunger', 'Water', 'Crisis', 'Global', 'Local ', 'Market', 'Food', 'Feed', 'Family ', 'Single', 'Friendly', 'Radical', 'The Planet as', 'Night Vision', 'Puzzelmaker', 'Flying Cars', 'Ego', 'Killer', 'Heaven', 'Sunrise', 'Turn On', 'Tune In', 'Drop Out', 'Politics', 'Anti', 'Pro', 'Movement', 'Culture', 'Art', 'Counterculture', 'Climate', 'cool', 'high quality', 'Communalism', 'sharing Economy', 'democratic', 'Tool', 'Learning', 'Prototyping', 'Future', 'Publishing', 'Fresh', 'Biosphere', 'The Computer as', 'Cultivating', 'Spaceship Earth', 'Software', 'Mother', 'Father', 'Friends', 'Gene', 'Cybernetics', 'Independent', 'Low Cost', 'Offline', 'Moment Village', 'flying Balloons', 'Instant', 'Librarys', 'Bootleg', 'Architecture', 'Architects', 'do-it-yourself', 'Geodesic', 'Geodesic Dome', 'Desks', 'Movable', 'Revolution', 'Withput', 'Designer', 'Design', 'Cabin', 'Money', 'in the Desert', 'Alternative', 'Organs', 'Media', 'Internet', 'Milk & Honey', 'Scrolling']
     }
   },
   components: {
@@ -43,7 +43,9 @@ export default {
   .slotmachine
     width 80vw
     margin 1rem auto
-    font-size 2rem
+    margin-top 30vh
+    font-size 4rem
+    cursor:  url('../assets/coin.png') 60 60, progress
   .slots
     width 100%
     display flex
